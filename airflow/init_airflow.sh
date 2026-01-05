7315398387a2d4a8ef4313783b07a64ee63f4d25
codrutciulacu <codrut.c.ciulacu@gmail.com>
2026-01-05 19:25:54 +0200

    Correct issues with the Dockerfile

---
+++ airflow/init_airflow.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+airflow db migrate
+
+airflow users create \
+  --username admin \
+  --password admin \
+  --firstname Admin \
+  --lastname User \
+  --role Admin \
+  --email admin@example.com || true
